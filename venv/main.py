ed2f5c7822d535d203afcc0366c524dc77867d2e
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-02-23 21:35:50 -0300

    Creando nuevas rutas con parametros de consulta

--- venv/main.py
+++ venv/main.py
@@ -12,3 +12,11 @@ def read_root():
 @app.get('/hola')
 def hola_mundo():
     return {'Hola':'Mundo! 2'}
+
+@app.get('/items/{item_id}')
+def read_id(item_id: int, q: Union[str,None]=None):
+    return {'item_id': item_id, 'q':q}
+
+@app.get('/calculadora')
+def calcular(operando1:float,operando2:float):
+    return {'suma': operando1+operando2}
