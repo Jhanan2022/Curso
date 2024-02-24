61a1959dd6d7d18f3c2f874c3e1968bed9719075
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-02-23 21:48:00 -0300

    Creado el modelo item con pydantic

--- venv/main.py
+++ venv/main.py
@@ -2,8 +2,14 @@
 # correr la app uvicorn main:app --reload --port 5000
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get('/')
 def read_root():
