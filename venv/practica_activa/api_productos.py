8903ceb4588eaac76e86ff30824a818cdaefb5f2
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-02-25 21:54:04 -0300

    Ya se realizó la api para CRUD

--- venv/practica_activa/api_productos.py
+++ venv/practica_activa/api_productos.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from uuid import uuid4 as uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 
 class Producto(BaseModel):
@@ -30,3 +30,37 @@ def crear_producto(producto:Producto):
     producto.id = str(uuid())
     productos.append(producto)
     return {'mensaje':'Producto creado satisfactoriamente.'}
+
+@app.get('/producto/{producto_id}')
+def obtener_producto_por_id(producto_id:str):
+    """ opcion 1
+        for p in productos:
+        if p.id == producto_id:
+            return p """
+    resultado = list(filter(lambda p:p.id == producto_id, productos))
+    if len(resultado):
+        return resultado[0]
+    raise HTTPException(status_code=404, detail=f'El producto con el ID: {producto_id}, no fue encontrado.')
+
+@app.delete('/producto/{producto_id}')
+def eliminar_producto_por_id(producto_id:str):
+    resultado = list(filter(lambda p:p.id == producto_id, productos))
+    if len(resultado):
+        producto = resultado[0]
+        productos.remove(producto)
+        return {'mensaje':f'El producto con el ID: {producto_id}, fue eliminado.'}
+
+    raise HTTPException(status_code=404, detail=f'El producto con el ID: {producto_id}, no fue encontrado.')
+
+@app.put('/producto/{producto_id}')
+def actualizar_producto_por_id(producto_id:str, producto:Producto):
+    resultado = list(filter(lambda p:p.id == producto_id, productos))
+    if len(resultado):
+        producto_encontrado = resultado[0]
+        producto_encontrado.nombre = producto.nombre
+        producto_encontrado.precio_compra = producto.precio_compra
+        producto_encontrado.precio_venta = producto.precio_venta
+        producto_encontrado.proveedor = producto.proveedor
+        
+        return producto_encontrado
+    raise HTTPException(status_code=404, detail=f'El producto con el ID: {producto_id}, no fue encontrado.')
