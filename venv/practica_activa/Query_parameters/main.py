f7a4c97dbb43e2415f025308eea8ca5ccc1a51fb
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-02-28 23:18:17 -0300

    Clase de hoy

--- venv/practica_activa/Query_parameters/main.py
+++ venv/practica_activa/Query_parameters/main.py
@@ -1,5 +1,5 @@
 """
-para commit mas rapidos, Ctrl + S x2, 
+para commit mas rapidos, Ctrl +1, Ctrl + 2, Ctrl + 3 
 #Query parameters modificado haciendo pasos
 from fastapi import FastAPI
 
@@ -12,7 +12,7 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 async def read_item(start: int = 0, end: int = 10, skip: int = 0):
     return fake_items_db[start : start + end: skip]
 """
-"""#query parameters"""
+"""#query parameters
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -23,3 +23,34 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+    """
+"""#Optional parameters
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id}   
+"""
+"""
+#Query parameter type conversion
+"""
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
