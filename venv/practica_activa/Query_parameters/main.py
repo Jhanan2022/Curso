ef2b6d323c5def193c38610347296d30251ea2d5
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-02-29 22:19:44 -0300

    Clase del día

--- venv/practica_activa/Query_parameters/main.py
+++ venv/practica_activa/Query_parameters/main.py
@@ -38,7 +38,7 @@ async def read_item(item_id: str, q: str | None = None):
 """
 """
 #Query parameter type conversion
-"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -53,4 +53,35 @@ async def read_item(item_id: str, q: str | None = None, short: bool = False):
         item.update(
             {"description": "This is an amazing item that has a long description"}
         )
+    return item
+"""
+"""
+#Multiple path and query parameters
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+"""
+"""
+#Required query parameters
+"""
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
     return item
