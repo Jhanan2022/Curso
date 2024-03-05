2bdf2f143297a5d36bba25df886515f80c4d9296
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-03-04 23:02:44 -0300

    Clase del dia

--- venv/practica_activa/Query_parameters/main.py
+++ venv/practica_activa/Query_parameters/main.py
@@ -75,7 +75,6 @@ async def read_user_item(user_id: int, item_id: str, q: str | None = None, short
 """
 """
 #Required query parameters
-"""
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -84,4 +83,14 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_user_item(item_id: str, needy: str):
     item = {"item_id": item_id, "needy": needy}
+    return item
+""" #Required optional default
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
+    item = {"item_id": item_id, "needy": needy, "skip":skip,"limit":limit}
     return item
