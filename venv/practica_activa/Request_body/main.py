fea60217bd90e21702c533bbb4ef9fd9d0d2a958
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-03-05 22:12:47 -0300

    clase del dia

--- venv/practica_activa/Request_body/main.py
+++ venv/practica_activa/Request_body/main.py
@@ -1,7 +1,4 @@
 """#Import pydantic
-"""
-"""
-"""
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -19,3 +16,67 @@ app = FastAPI()
 @app.post("/items/")
 async def create_item(item: Item):
     return item
+"""
+"""#Usar el Modelo
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = (item.price + item.tax)
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+"""
+""" #Request Body + path parameters
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+"""
+#Request body + path + query parameters
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
