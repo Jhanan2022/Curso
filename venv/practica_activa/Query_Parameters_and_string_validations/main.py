8872cd3f7dd30e837e7ffc18bf1a2a8bdc0b0f9b
YJVS Creator <116447131+Jhanan2022@users.noreply.github.com>
2024-03-07 22:20:39 -0300

    0703

--- venv/practica_activa/Query_Parameters_and_string_validations/main.py
+++ venv/practica_activa/Query_Parameters_and_string_validations/main.py
@@ -1,11 +1,14 @@
+from typing import Annotated
+
 from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-
 @app.get("/items/")
-async def read_items(q: str | None = Query(default=None, max_length=50)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+async def read_items(
+    q: Annotated[str | None, Query(min_length=3, max_length=20)] = None
+):
+    results  = {"items": [{"item_id":"foo"},{"item_id":"bar"}]}
     if q:
-        results.update({"q": q})
+        results.update({"q":q})
     return results
